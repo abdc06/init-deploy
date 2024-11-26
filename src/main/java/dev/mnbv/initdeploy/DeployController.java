71b37b354d73e8b480dea45c84e8535d2a6ffce6
kim <kim@kims-MacBook-Pro.local>
2024-11-26 20:25:51 +0900

    view and message test

--- src/main/java/dev/mnbv/initdeploy/DeployController.java
+++ src/main/java/dev/mnbv/initdeploy/DeployController.java
@@ -1,13 +1,20 @@
 package dev.mnbv.initdeploy;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class DeployController {
 
     @GetMapping("/")
-    public String hello () {
+    public String index () {
+        return "/index";
+    }
+
+    @GetMapping("/api")
+    @ResponseBody
+    public String api () {
         return "Deployment was successful.";
     }
 }
